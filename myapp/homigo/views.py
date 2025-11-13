71dc7818563413999a063df8c827aa36527f78cc
AKS-kumar-np <221376336+AKS-kumar-np@users.noreply.github.com>
2025-11-13 11:51:56 +0000

    added deployment server

--- myapp/homigo/views.py
+++ myapp/homigo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'aawas/index.html')
+    return render(request, 'index.html')
+
+def health(request):
+    return HttpResponse('ok')
--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -9,26 +9,42 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
-import os
+from decouple import config
+from django.core.management.utils import get_random_secret_key
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+REPO_DIR = BASE_DIR.parent
+TEMPLATES_DIR =  BASE_DIR / 'templates'
+TEMPLATES_DIR.mkdir(parents=True, exist_ok=True)
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str, default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    #"homigo.com",
+    #"www.homigo.com",
+    ".railway.app"
+]
 
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.railway.app"
+
+]
+
+if DEBUG:
+    ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -57,7 +73,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
